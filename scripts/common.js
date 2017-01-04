4d7f840edfbb3faefed32ce301efda0206c79b7e
ashwin-pc <ashwinpc1993@gmail.com>
2017-01-05 00:45:45 +0530

    Graphics initial layout

--- scripts/common.js
+++ scripts/common.js
@@ -2,6 +2,7 @@
 function nextSection() {
   var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
   var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  var scrollVal = h * (Math.floor(scrollTop/h) + 1) + 4;
+  var page = (Math.floor(scrollTop/h) + 1);
+  var scrollVal = h *  page;
   zenscroll.toY(scrollVal);
 }
