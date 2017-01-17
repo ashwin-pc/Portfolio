a19f2439ea2e7b824da6e8400aaacce3ce27c9d6
ashwin-pc <ashwinpc1993@gmail.com>
2017-01-18 01:07:50 +0530

    web design integrated

---
+++ src/js/1_initialize.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDFUrgN8V-HKvI7W_omYzph_HUWYT_3qc0",
+  authDomain: "portfolio-50069.firebaseapp.com",
+  databaseURL: "https://portfolio-50069.firebaseio.com",
+  storageBucket: "portfolio-50069.appspot.com",
+  messagingSenderId: "620857279220"
+};
+firebase.initializeApp(config);
+
+// Initialize references
+var dbRefWebsites = firebase.database().ref().child('websites');
