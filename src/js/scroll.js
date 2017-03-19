d9ebc3e47d4f048c03be910a177607ffb922b173
ashwin-pc <ashwinpc1993@gmail.com>
2017-03-19 17:57:23 +0530

    added up and down key shortcuts

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -2,16 +2,34 @@
 var sections = document.getElementsByClassName("page-section");
 
 function nextSection() {
+  var currentSectionIndex = getCurrentSectionIndex();
+
+  // Scroll location
+  var nextIndex = (currentSectionIndex == sections.length1-1) ? currentSectionIndex : currentSectionIndex + 1;
+  zenscroll.to(sections[nextIndex]);
+}
+
+function previousSection() {
+  var currentSectionIndex = getCurrentSectionIndex();
+
+  // Scroll location
+  var prevIndex = (currentSectionIndex == 0) ? 0 : currentSectionIndex-1;
+  // console.log(prevIndex, i);
+  zenscroll.to(sections[prevIndex]);
+}
+
+function getCurrentSectionIndex() {
   var scrollTop = (window.pageYOffset || document.documentElement.scrollTop) + 1;
+  var currentSectionIndex = 0;
 
   // Scroll Logic
   for (var i = 0; i < sections.length; i++) {
-    if (sections[i].offsetTop > (scrollTop + 5)) {
-      // Scroll location
-      zenscroll.to(sections[i]);
-      break;
+    if (sections[i].offsetTop < (scrollTop + 5)) {
+      currentSectionIndex = i;
     }
   }
+
+  return currentSectionIndex;
 }
 
 // nextSection Keyboard shortcut
@@ -19,8 +37,11 @@ document.onkeydown = function (e) {
     e = e || window.event;
 
     if (e.keyCode == '40') {
-        // down Arrow
-        nextSection();
+      // Down Arrow
+      nextSection();
+    } else if (e.keyCode == '38') {
+      // Up Arrow
+      previousSection();
     }
 }
 
