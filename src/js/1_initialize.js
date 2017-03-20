b3f33b8fd65eba7d7aebd1514e20b9f1018726b5
ashwin-pc <ashwinpc1993@gmail.com>
2017-03-20 23:19:00 +0530

    replaced firebase config with simple ajax

--- src/js/1_initialize.js
+++ src/js/1_initialize.js
@@ -1,16 +1,5 @@
 // Initialize Firebase
-var config = {
-  apiKey: "AIzaSyDFUrgN8V-HKvI7W_omYzph_HUWYT_3qc0",
-  authDomain: "portfolio-50069.firebaseapp.com",
-  databaseURL: "https://portfolio-50069.firebaseio.com",
-  storageBucket: "portfolio-50069.appspot.com",
-  messagingSenderId: "620857279220"
-};
-firebase.initializeApp(config);
-
-// Initialize references
-var dbRefWebsites = firebase.database().ref().child('websites');
-var dbRefGraphics = firebase.database().ref().child('graphics');
+var firebaseBaseUrl = "https://portfolio-50069.firebaseio.com/";
 
 // Ajax
 function $ajax(ob, callback) {
