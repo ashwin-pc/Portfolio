05495c8805e55e83bbde01bc4b6169a40858b7e1
ashwin-pc <ashwinpc1993@gmail.com>
2017-03-30 18:09:03 +0530

    added gravity

--- src/js/0_common.js
+++ src/js/0_common.js
@@ -15,11 +15,16 @@ function ToastContainer(options) {
   // TODO : Gravity (add to comments the options and impliment)
   // TODO : Remove styles from css if possible
   // Append Toast container to page
-  var containerEle = document.createElement("div");
-  containerEle.id = "toast-container";
-  document.body.appendChild(containerEle);
+  var containerTopEle = document.createElement("div");
+  containerTopEle.id = "toast-container-top";
+  document.body.appendChild(containerTopEle);
+  
+  var containerBottomEle = document.createElement("div");
+  containerBottomEle.id = "toast-container-bottom";
+  document.body.appendChild(containerBottomEle);
 
-  this.toastContainerEle = containerEle;
+  this.topToastContainerEle = containerTopEle;
+  this.bottomToastContainerEle = containerBottomEle;
   this.message = options.message || "Something went wrong, Try Again";
   this.timeout = options.timeout || 10000;
   this.logging = options.logging || false;
@@ -36,13 +41,15 @@ ToastContainer.prototype.toast = function (msg, options, callback) {
     // Options
     var m     = msg || this.message;
     var t     = (options && options.timeout) ? options.timeout : this.timeout;
+    var g     = (options && options.gravity) ? options.gravity : this.gravity;
     var log   = (options && typeof options.logging !== 'undefined') ? options.logging : this.logging;
 
     // Create HTML
-    var containerEle = this.toastContainerEle;
+    var containerEle = (g === "top") ? this.topToastContainerEle : this.bottomToastContainerEle;
     var toastEle = document.createElement("div");
     toastEle.classList.add("toast");
     toastEle.innerHTML = m;
+    toastEle.classList.add(g);
     containerEle.appendChild(toastEle);
 
     // Slide in toast
@@ -58,7 +65,7 @@ ToastContainer.prototype.toast = function (msg, options, callback) {
     // Remove from DOM
     setTimeout(function() {
         containerEle.removeChild(toastEle);
-    }, t+1000);
+    }, t+700);
 
     // Log Error
     if (log) {
--- src/js/1_initialize.js
+++ src/js/1_initialize.js
@@ -10,4 +10,6 @@ if (document.documentElement.clientWidth < 480) {
 
 if (!_mobile) {
     toastContainer.toast("Use arrow keys to navigate throught sections", {logging:false, timeout:3000});
+    toastContainer.toast("Use arrow keys to navigate throught sections", {logging:false, timeout:4000});
+    toastContainer.toast("Use arrow keys to navigate throught sections", {logging:false, timeout:5000});
 }
